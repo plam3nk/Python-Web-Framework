5d387162b8ebd996f04bae727d21e2c50a5a9b40
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-07-17 23:22:01 +0300

    exercise

--- Django Signals Middleware Cache Sessions/django_web_tools/django_web_tools/web/views.py
+++ Django Signals Middleware Cache Sessions/django_web_tools/django_web_tools/web/views.py
@@ -1,13 +1,25 @@
 import random
 
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
-from django.views.decorators import cache
+from django.core.cache import cache
+
+# from django.views.decorators import cache
+
+UserModel = get_user_model()
 
 
 # Create your views here.
-@cache.cache_page(5)
+# @cache.cache_page(5 * 60)
 def index(request):
+    if not cache.get('users'):
+        cache.set('users', UserModel.objects.all(), 10)
+
+    users = cache.get('users')
+
     context = {
-        'count': random.randint(1, 100000)
+        'count': random.randint(1, 100000),
+        'users': users,
     }
+
     return render(request, 'index.html', context=context)
