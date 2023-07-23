8d3bef3720a2906538a0ccc2fd4e63255d266e4a
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-07-23 20:34:56 +0300

    removed old dir

--- Workshops/Workshop Part 1/petstagram/petstagram/pets/utils.py
+++
@@ -1,6 +0,0 @@
-from petstagram.pets.models import Pet
-
-
-def get_pet_by_name_and_username(pet_slug):
-    # TODO: fix 'username' when authentication is done.
-    return Pet.objects.filter(slug=pet_slug).first()
